52b8ad6397dd63917e33eb1329ecf3046920a28b
Victoria Mount <victoriamount01@gmail.com>
2020-10-30 10:42:41 -0500

    Complete up to testing

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,14 @@
 // write your custom hook here to control your checkout form
+
+import { useState } from 'react'
+
+export const useForm = initialValue => {
+    const [value, setValue] = useState(initialValue)
+
+    const handleChanges = (e) => {
+        setValue({ ...value, [e.target.name]: e.target.value });
+    };
+
+
+    return [value, setValue, handleChanges]
+}
